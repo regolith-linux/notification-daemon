896ec59e01490708d57c416a9c3e8f0d92ec1f3d
Mike Hearn <mike@navi.cx>
2004-07-04 19:25:05 +0000

    add todo list to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,18 @@
  * License along with this program; if not, write to the Free
  * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA  02111-1307  USA
+ *
+ *
+ * TODO:
+ *  required:
+ *    - caps query
+ *    - timeouts
+ *    - image support
+ *  optional:
+ *    - popup sliding/notification
+ *    - animated images
+ *    - KNotify proxy
+ *
  */
 #ifdef HAVE_CONFIG_H
 # include "config.h"
