62cf348a158d32c2c64acf2bce7799a926c544f3
Christian Hammond <chipx86@chipx86.com>
2005-07-13 16:59:51 +0000

    Patch by Richard Hughes to fix another build issue with D-BUS 0.3x.

--- src/Notification.cc
+++ src/Notification.cc
@@ -19,6 +19,10 @@
  * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA  02111-1307  USA
  */
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include "Notification.hh"
 #include "logging.hh"
 #include "dbus-compat.h"
