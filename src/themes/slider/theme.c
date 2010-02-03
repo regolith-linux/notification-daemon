01268d5bc52ba2f3f94b65e7ebacf6f44bbb9b1f
Javier Jardón <jjardon@gnome.org>
2010-02-03 03:54:23 +0100

    [themes] Remove deprecated GTK+ symbol
    Substitute gtk_rc_style_unref () with g_object_unref ()

--- src/themes/slider/theme.c
+++ src/themes/slider/theme.c
@@ -551,7 +551,7 @@ create_notification (UrlClickedCb url_clicked)
         rcstyle = gtk_rc_style_new ();
         rcstyle->xthickness = rcstyle->ythickness = 0;
         gtk_widget_modify_style (close_button, rcstyle);
-        gtk_rc_style_unref (rcstyle);
+        g_object_unref (rcstyle);
 
         atkobj = gtk_widget_get_accessible (close_button);
         atk_action_set_description (ATK_ACTION (atkobj), 0,
