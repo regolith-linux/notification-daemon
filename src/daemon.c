c72adfb4a43613834fae6c54b25adb296d3884dd
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2015-09-16 03:11:49 +0300

    internationalization: fix locale dir

--- src/daemon.c
+++ src/daemon.c
@@ -371,7 +371,7 @@ main (int argc, char **argv)
         NotifyDaemon *daemon;
         guint         owner_id;
 
-        bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+        bindtextdomain (GETTEXT_PACKAGE, LOCALE_DIR);
         bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
         textdomain (GETTEXT_PACKAGE);
 
